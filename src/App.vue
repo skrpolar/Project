0be324d19f857180dfd1a319d4239df2e05247a9
Guohong Liu <shokorapolar@yahoo.com>
2017-07-31 19:08:31 +0800

    update tamplate struct.
    somthing error in animate (css height).

--- src/App.vue
+++ src/App.vue
@@ -6,69 +6,43 @@
       <div class="content">
         <div class="left_nav">
           <ul>
-            <li class="level1 level_plus" @click="navActive.menu1=!navActive.menu1">
-              <div>
-                <span :class="{ plus:!navActive.menu1, sub: navActive.menu1 }">^</span>
-                {{ $t('messages.menu1') }}
+            <div v-for="(value, key, index) in navInit">
+              <div v-if="value.hasNext">
+                <li :class="{ level1: true, level_plus:value.hasNext }" @click="value.navActive=!value.navActive">
+                  <div>
+                    <span :class="[ value.navActive ? 'sub' : 'plus' ]">^</span>
+                    {{ $t(`messages.${key}`) }}
+                  </div>
+                </li>
+                <ul :class="ulClass(value)">
+                  <!-- 控制菜单方式由v-if更改为css样式实现动画效果 -->
+                  <li v-for="(value2, key2, index2) in value.next" :class="{ level2:true, li_active:liIndex==value2.liActive }" @click="liIndex=value2.liActive">
+                    <div v-if="value2.hasNext">
+                      <li :class="{ level2: true, level_plus:value2.hasNext }" @click="value2.navActive=!value2.navActive">
+                        <div>
+                          <span :class="[ value2.navActive ? 'sub' : 'plus' ]">^</span>
+                          <span>{{ $t(`messages.${key2}`) }}</span>
+                        </div>
+                      </li>
+                      <ul :class="ulClass(value2)">
+                        <li v-for="(value3, key3, index3) in value2.next" :class="{ level3:true, li_active:liIndex==value3.liActive }" @click="liIndex=value3.liActive">
+                          <router-link :title="$t(`messages.${key3}`)" :to="{ path: value3.path }">{{ $t(`messages.${key3}`) }}</router-link>
+                        </li>
+                      </ul>
+                    </div>
+                    <div v-else>
+                      <router-link :title="$t(`messages.${key2}`)" :to="{ path: value2.path }">{{ $t(`messages.${key2}`) }}</router-link>
+                    </div>
+                  </li>
+                </ul>
               </div>
-            </li>
-            <ul :class="{ level_ul:true, open3:navActive.menu1 }"> <!-- 控制菜单方式由v-if更改为css样式实现动画效果 -->
-              <li :class="{ level2:true, li_active:liIndex==liActive['menu1_1'] }" @click="liIndex=liActive['menu1_1']">
-                <router-link :title="$t('messages.menu1_1')" :to="{ path: '/' }">{{ $t('messages.menu1_1') }}</router-link>
-              </li>
-              <li :class="{ level2:true, li_active:liIndex==liActive['menu1_2'] }" @click="liIndex=liActive['menu1_2']">
-                <router-link :title="$t('messages.menu1_2')" :to="{ path: '/content2' }">{{ $t('messages.menu1_2') }}</router-link>
-              </li>
-              <li :class="{ level2:true, li_active:liIndex==liActive['menu1_3']} " @click="liIndex=liActive['menu1_3']">
-                <router-link :title="$t('messages.menu1_3')" :to="{ path: `/content3` }">{{ $t('messages.menu1_3') }}</router-link>
-              </li>
-            </ul>
-            <li class="level1 level_plus" @click="navActive.menu2=!navActive.menu2">
-              <div>
-                <span :class="{ plus:!navActive.menu2, sub: navActive.menu2 }">^</span>
-                {{ $t('messages.menu2') }}
+              <div v-else>
+                <router-link :title="$t(`messages.${key}`)" :to="{ path: value.path }">{{ $t(`messages.${key}`) }}</router-link>
               </div>
-            </li>
-            <ul :class="{ level_ul:true, open1:navActive.menu2 }">
-              <li :class="{ level2:true, li_active:liIndex==liActive['menu2_1'] }" @click="liIndex=liActive['menu2_1']">
-                <router-link :title="$t('messages.menu2_1')" :to="{ path: `/content4` }">{{ $t('messages.menu2_1') }}</router-link>
-              </li>
-            </ul>
-            <li class="level1 level_plus" @click="navActive.menu3=!navActive.menu3">
-              <div>
-                <span :class="{ plus:!navActive.menu3, sub: navActive.menu3 }">^</span>
-                {{ $t('messages.menu3') }}
-              </div>
-            </li>
-            <ul :class="{ level_ul:true, open1:navActive.menu3 }">
-              <li :class="{ level2:true, li_active:liIndex==liActive['menu3_1'] }" @click="liIndex=liActive['menu3_1']">
-                <router-link :title="$t('messages.menu3_1')" :to="{ path: `/content5` }">{{ $t('messages.menu3_1') }}</router-link>
-              </li>
-            </ul>
-            <li class="level1 level_plus" @click="navActive.menu4=!navActive.menu4">
-              <div>
-                <span :class="{ plus:!navActive.menu4, sub: navActive.menu4 }">^</span>
-                {{ $t('messages.menu4') }}
-              </div>
-            </li>
-            <ul :class="{ level_ul:true, open1:navActive.menu4 }">
-              <li :class="{ level2:true, li_active:liIndex==liActive['menu4_1'] }" @click="liIndex=liActive['menu4_1']">
-                <router-link :title="$t('messages.menu4_1')" :to="{ path: `/content6` }">{{ $t('messages.menu4_1') }}</router-link>
-              </li>
-            </ul>
-            <li class="level1 level_plus" @click="navActive.menu5=!navActive.menu5">
-              <div>
-                <span :class="{ plus:!navActive.menu5, sub: navActive.menu5 }">^</span>
-                {{ $t('messages.menu5') }}
-              </div>
-            </li>
-            <ul :class="{ level_ul:true, open1:navActive.menu5 }">
-              <li :class="{ level2:true, li_active:liIndex==liActive['menu5_1'] }" @click="liIndex=liActive['menu5_1']">
-                <router-link :title="$t('messages.menu5_1')" :to="{ path: `/content7` }">{{ $t('messages.menu5_1') }}</router-link>
-              </li>
-            </ul>
+            </div>
           </ul>
         </div>
+  
         <div class="right_rev">
           <div class="search">
             <input type="text" id="search_input" @keyup.enter="searchFunc()" :class="{ ser_input:!searchPng, ser_input_active:searchPng }" :placeholder="$t('search.msg')">
@@ -76,6 +50,7 @@
           </div>
           <router-view :locale="locale"></router-view>
         </div>
+  
         <div class="oTop">
           <div>^</div>
         </div>
@@ -148,50 +123,146 @@ export default {
       isEmputy: false,
       locale: localStorage.locale,
       searchPng: false,
-      /* 模板结构化，v-for有问题。
+      // 模板结构化
       navInit: {
         menu1: {
+          text: {
+            ch: '开始前必读',
+            en: 'Start'
+          },
+          navActive: false,
           hasNext: true,
-          navActive: true,
+          nextLength: 4,
           next: {
             menu1_1: {
+              text: {
+                ch: '介绍',
+                en: 'Introduction'
+              },
               hasNext: false,
               path: '/',
               liActive: 11
             },
 
             menu1_2: {
+              text: {
+                ch: '公众号接口权限说明',
+                en: 'Public address interface'
+              },
               hasNext: false,
-              path: '/index',
+              path: '/content2',
               liActive: 12
             },
 
             menu1_3: {
-              hasNext: false,
-              path: '/content3',
-              liActive: 13
+              text: {
+                ch: '入门指引',
+                en: 'Getting Started Guide'
+              },
+              hasNext: true,
+              navActive: false,
+              nextLength: 1,
+              // path: '/content3',
+              next: {
+                menu1_3_1: {
+                  text: {
+                    ch: '',
+                    en: ''
+                  },
+                  hasNext: false,
+                  liActive: 131,
+                  path: '/content3'
+                }
+              }
             }
           }
         },
 
         menu2: {
-          hasNext: false, // 只有hasNext为false时，有liActive属性，没有navActive属性
-          liActive: 2
+          text: {
+            ch: '开始开发',
+            en: 'head2'
+          },
+          navActive: false,
+          hasNext: true, // 只有hasNext为false时，有liActive属性，没有navActive属性
+          nextLength: 1,
+          next: {
+            menu2_1: {
+              text: {
+                ch: '内容四',
+                en: 'Content4'
+              },
+              hasNext: false,
+              path: '/content4',
+              liActive: 21
+            }
+          }
         },
 
         menu3: {
-          hasNext: true,
+          text: {
+            ch: '自定义菜单',
+            en: 'MainBody'
+          },
           navActive: false,
+          hasNext: true,
+          nextLength: 1,
           next: {
             menu3_1: {
+              text: {
+                ch: '内容五',
+                en: 'content5'
+              },
               hasNext: false,
-              path: '/',
+              path: '/content5',
               liActive: 31
             }
           }
-        }
+        },
+
+        menu4: {
+          text: {
+            ch: '素材管理',
+            en: 'Footer'
+          },
+          navActive: false,
+          hasNext: true,
+          nextLength: 1,
+          next: {
+            menu4_1: {
+              text: {
+                ch: '内容六',
+                en: 'content6'
+              },
+              hasNext: false,
+              path: '/content6',
+              liActive: 41
+            }
+          }
+        },
+
+        menu5: {
+          text: {
+            ch: '用户管理',
+            en: 'Footer2'
+          },
+          navActive: false,
+          hasNext: true,
+          nextLength: 1,
+          next: {
+            menu5_1: {
+              text: {
+                ch: '内容七',
+                en: 'content7'
+              },
+              hasNext: false,
+              path: '/content7',
+              liActive: 51
+            }
+          }
+        },
       },
-      */
+
       liIndex: sessionStorage.index,
       liActive: {
         menu1_1: 11,
@@ -211,6 +282,7 @@ export default {
       }
     }
   },
+
   watch: {
     locale: function (val) {
       this.$i18n.locale = val;
@@ -223,6 +295,10 @@ export default {
     sessionStorage: function (val) {
       console.log(1);
     }
+  },
+
+  created() {
+    // this.$data.liActive['menu1_1'] = 12;
   },
 
   mounted() {
@@ -305,21 +381,20 @@ export default {
       }
     }
 
-    this.liIndex = sessionStorage.index; // 从路由传正确的li index
-    for (var i in this.navActive) {
+    // this.liIndex = sessionStorage.index; // 从路由传正确的li index
+    for (var i in this.navInit) {
       if (i == sessionStorage.navIndex) {
         this.navActive[i] = true;
       }
     }
 
-    
+
   },
 
   beforeUpdate() {
     // 通过导入来搜索内容
     var s = index;
     var input = document.getElementById('search_input');
-    console.log(input);
   },
 
   updated() {
@@ -357,6 +432,12 @@ export default {
     // }
     searchFunc: function () {
       this.$router.push({ path: '/content2' })
+    },
+    ulClass: function (val) {
+      var o = { level_ul: true };
+      // o[`open${val.nextLength}`] = val.navActive;
+      o['open'] = val.navActive;
+      return o;
     }
   }
 }
@@ -382,6 +463,86 @@ export default {
 //     console.log(this.$refs.p.$data.msg); // 获取子组件的data值，可以用来搜索子组件的信息。
 //   }
 // }
+
+/*
+<div>
+              <li class="level1 level_plus" @click="navActive.menu1=!navActive.menu1">
+                <div>
+                  <span :class="{ plus:!navActive.menu1, sub: navActive.menu1 }">^</span>
+                  {{ $t('messages.menu1') }}
+                </div>
+              </li>
+              <ul :class="{ level_ul:true, open3:navActive.menu1 }"> <!-- 控制菜单方式由v-if更改为css样式实现动画效果 -->
+                <li :class="{ level2:true, li_active:liIndex==liActive['menu1_1'] }" @click="liIndex=liActive['menu1_1']">
+                  <router-link :title="$t('messages.menu1_1')" :to="{ path: '/' }">{{ $t('messages.menu1_1') }}</router-link>
+                </li>
+                <li :class="{ level2:true, li_active:liIndex==liActive['menu1_2'] }" @click="liIndex=liActive['menu1_2']">
+                  <router-link :title="$t('messages.menu1_2')" :to="{ path: '/content2' }">{{ $t('messages.menu1_2') }}</router-link>
+                </li>
+                <li :class="{ level2:true, li_active:liIndex==liActive['menu1_3']} " @click="liIndex=liActive['menu1_3']">
+                  <router-link :title="$t('messages.menu1_3')" :to="{ path: `/content3` }">{{ $t('messages.menu1_3') }}</router-link>
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <li class="level1 level_plus" @click="navActive.menu2=!navActive.menu2">
+              <div>
+                <span :class="{ plus:!navActive.menu2, sub: navActive.menu2 }">^</span>
+                {{ $t('messages.menu2') }}
+              </div>
+            </li>
+            <ul :class="{ level_ul:true, open1:navActive.menu2 }">
+              <li :class="{ level2:true, li_active:liIndex==liActive['menu2_1'] }" @click="liIndex=liActive['menu2_1']">
+                <router-link :title="$t('messages.menu2_1')" :to="{ path: `/content4` }">{{ $t('messages.menu2_1') }}</router-link>
+              </li>
+            </ul>
+            </div>
+
+            <div>
+              <li class="level1 level_plus" @click="navActive.menu3=!navActive.menu3">
+              <div>
+                <span :class="{ plus:!navActive.menu3, sub: navActive.menu3 }">^</span>
+                {{ $t('messages.menu3') }}
+              </div>
+            </li>
+            <ul :class="{ level_ul:true, open1:navActive.menu3 }">
+              <li :class="{ level2:true, li_active:liIndex==liActive['menu3_1'] }" @click="liIndex=liActive['menu3_1']">
+                <router-link :title="$t('messages.menu3_1')" :to="{ path: `/content5` }">{{ $t('messages.menu3_1') }}</router-link>
+              </li>
+            </ul>
+            </div>
+
+            <div>
+              <li class="level1 level_plus" @click="navActive.menu4=!navActive.menu4">
+              <div>
+                <span :class="{ plus:!navActive.menu4, sub: navActive.menu4 }">^</span>
+                {{ $t('messages.menu4') }}
+              </div>
+            </li>
+            <ul :class="{ level_ul:true, open1:navActive.menu4 }">
+              <li :class="{ level2:true, li_active:liIndex==liActive['menu4_1'] }" @click="liIndex=liActive['menu4_1']">
+                <router-link :title="$t('messages.menu4_1')" :to="{ path: `/content6` }">{{ $t('messages.menu4_1') }}</router-link>
+              </li>
+            </ul>
+            </div>
+            
+            <div>
+              <li class="level1 level_plus" @click="navActive.menu5=!navActive.menu5">
+              <div>
+                <span :class="{ plus:!navActive.menu5, sub: navActive.menu5 }">^</span>
+                {{ $t('messages.menu5') }}
+              </div>
+            </li>
+            <ul :class="{ level_ul:true, open1:navActive.menu5 }">
+              <li :class="{ level2:true, li_active:liIndex==liActive['menu5_1'] }" @click="liIndex=liActive['menu5_1']">
+                <router-link :title="$t('messages.menu5_1')" :to="{ path: `/content7` }">{{ $t('messages.menu5_1') }}</router-link>
+              </li>
+            </ul>
+            </div>
+          </ul>
+        </div>
+*/
 </script>
 
 <style>
@@ -395,11 +556,10 @@ export default {
 
 
 
+
 /* -----左侧导航栏------*/
 
-@-moz-document url-prefix() {
-  
-}
+@-moz-document url-prefix() {}
 
 .plus {
   float: right;
@@ -433,11 +593,11 @@ export default {
   width: 100%;
 }
 
-.left_nav ul:first-child {
+.left_nav>ul:first-of-type {
   margin-top: 0.25rem;
 }
 
-.left_nav ul:last-child {
+.left_nav>ul:last-of-type {
   margin-bottom: 0.25rem;
 }
 
@@ -501,18 +661,23 @@ export default {
   -webkit-animation: fade-in .35s;
 }
 
-.left_nav .li_active{
+.left_nav .li_active {
   /* border-right: 0.03rem solid #369fe8; */
 }
 
 .left_nav .li_active a {
   color: #369fe8;
-   border-right: .04rem solid #368fe8; /* 368fe8 */ 
+  border-right: .04rem solid #368fe8;
 }
 
 .left_nav .level2 a {
   padding-left: 0.55rem;
 }
+
+.left_nav .level2 div {
+  /* padding-left: 0.2rem; */
+} 
+
 
 .left_nav .level3 a {
   padding-left: 0.65rem;
@@ -523,10 +688,14 @@ export default {
 }
 
 .left_nav .level_ul {
-  height: 0rem; 
+  height: 0;
   overflow: hidden;
-  transform: translate(0, 0); 
+  transform: translate(0, 0);
   transition: height .2s ease-in-out 0s;
+}
+
+.left_nav .open {
+  height: 100%;
 }
 
 .left_nav .open1 {
@@ -541,6 +710,73 @@ export default {
   height: 1.8rem;
 }
 
+.left_nav .open4 {
+  height: 2.4rem;
+}
+
+.left_nav .open5 {
+  height: 3.0rem;
+}
+
+.left_nav .open6 {
+  height: 3.6rem;
+}
+
+.left_nav .open7 {
+  height: 4.2rem;
+}
+
+.left_nav .open8 {
+  height: 4.8rem;
+}
+
+.left_nav .open9 {
+  height: 5.4rem;
+}
+
+.left_nav .open10 {
+  height: 6.0rem;
+}
+
+.left_nav .open11 {
+  height: 6.6rem;
+}
+
+.left_nav .open12 {
+  height: 7.2rem;
+}
+
+.left_nav .open13 {
+  height: 7.8rem;
+}
+
+.left_nav .open14 {
+  height: 8.4rem;
+}
+
+.left_nav .open15 {
+  height: 9.0rem;
+}
+
+.left_nav .open16 {
+  height: 9.6rem;
+}
+
+.left_nav .open17 {
+  height: 10.2rem;
+}
+
+.left_nav .open18 {
+  height: 10.8rem;
+}
+
+.left_nav .open19 {
+  height: 11.4rem;
+}
+
+.left_nav .open20 {
+  height: 12.0rem;
+}
 
 @keyframes fade-in {
   0% {
@@ -565,6 +801,10 @@ export default {
     background: #efefef;
   }
 }
+
+
+
+
 
 
 /*---------------------*/
@@ -601,6 +841,10 @@ export default {
 .oTop div:hover {
   color: white;
 }
+
+
+
+
 
 /* -----右侧显示栏------*/
 
@@ -649,18 +893,18 @@ export default {
   font-size: .2rem;
   padding: .1rem;
   color: #333F5C;
-  border: 0.01rem solid rgba(0,0,0,.1);
-  transition: margin .2s ease-in-out 0s,opacity .2s ease-out 0s;
-  -webkit-transition: margin .2s ease-in-out 0s,opacity .2s ease-out 0s;
-  -o-transition: margin .2s ease-in-out 0s,opacity .2s ease-out 0s;
-  -moz-transition: margin .2s ease-in-out 0s,opacity .2s ease-out 0s;
-   margin-top: .2rem;  
+  border: 0.01rem solid rgba(0, 0, 0, .1);
+  transition: margin .2s ease-in-out 0s, opacity .2s ease-out 0s;
+  -webkit-transition: margin .2s ease-in-out 0s, opacity .2s ease-out 0s;
+  -o-transition: margin .2s ease-in-out 0s, opacity .2s ease-out 0s;
+  -moz-transition: margin .2s ease-in-out 0s, opacity .2s ease-out 0s;
+  margin-top: .2rem;
   /* border: 0;  */
-  float:right; 
+  float: right;
 }
 
 .right_rev input:hover {
-  border: 0.01rem solid rgba(92,173,255,1);
+  border: 0.01rem solid rgba(92, 173, 255, 1);
 }
 
 .ser_input {
@@ -673,6 +917,10 @@ export default {
   margin-right: .6rem;
   opacity: .8
 }
+
+
+
+
 
 /*--------------------*/
 
